d812d37d8d2e7a85b4375a5d5709ed9cb1efaa6a
Ashborn <hosmellhahaha@gmail.com>
2026-09-01 17:19:32 -0500

    fix Vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
+import { cloudflare } from "@cloudflare/vite-plugin";
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), cloudflare()],
   build: {
     // Code splitting para optimizar carga inicial
     rollupOptions: {
